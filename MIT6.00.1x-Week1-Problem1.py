fbd81f03f65d7e28fefc213bfcc86f5c275b68ba
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2020-09-23 12:22:39 +0100

    Create MIT-6.00.1x-Exercises repo
    Create repo and Include Week 1 Problem 1 file

---
+++ MIT6.00.1x-Week1-Problem1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 12:13:47 2020
+
+@author: Atanas Kozarev
+
+Exam
+Problem 1 
+0.0/10.0 points (graded)
+Assume s is a string of lower case characters.
+Write a program that counts up the number of vowels contained in the string s. 
+Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. 
+For example, if s = 'azcbobobegghakl', your program should print:
+
+Number of vowels: 5
+
+
+"""
+# given s
+
+s = 'azcbobobegghakl'
+
+vowels = 0
+
+for aLetter in s:
+    if (aLetter == 'a') or (aLetter == 'e') or (aLetter == 'i')  or (aLetter == 'o') \
+    or  (aLetter == 'u'):
+        vowels += 1
+        
+print("Number of vowels: " + str(vowels))
