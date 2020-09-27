6efac06eaae6217957543067166a8bfccbd3bc6a
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2020-09-27 12:25:45 +0100

    Some HelloWorld python files from Lecture 1

---
+++ Lecture1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 10:05:14 2020
+
+@author: Atanas
+"""
+
+mysum = 0
+
+for i in range(5,11,2):
+    mysum = mysum + i
+    print(mysum)
+    if mysum == 5:
+        break
+    
+    
+        
---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 06:14:34 2020
+
+@author: student
+"""
+
+print('hello world')
+print('I like 6.00.1x!')
